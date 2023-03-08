779251e601cfcf3114a887309cbbffde9e41b490
Bharath Raj <bharathrajgngowdara@gmail.com>
2023-03-09 00:58:47 +0530

    feat: add user & auth joi validation middlewares

---
+++ src/middlewares/joiValidation.js
@@ -0,0 +1,17 @@
+const joiValidator = (joiSchema, validateProperty = 'body') => {
+  return async function (req, res, next) {
+    try {
+      const value = await joiSchema.validateAsync(req[validateProperty]);
+
+      // for more trustable validation
+      req[validateProperty] = value;
+      next();
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+module.exports = {
+  joiValidator,
+};

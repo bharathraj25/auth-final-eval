853d0d37c16c09c0ee24ace0d2247a74b25bfbe1
Bharath Raj <bharathrajgngowdara@gmail.com>
2023-03-10 15:55:10 +0530

    feat: add user services

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,11 +2,11 @@ const db = require('../../db/models');
 const utilService = require('../utils');
 
 module.exports = {
-  async createUser(username, password) {
+  async createUser(email, password) {
     const newUser = await db
       .Users
       .create({
-        username,
+        email,
         password: utilService.hashString(password)
       });
     delete newUser.dataValues.password;
